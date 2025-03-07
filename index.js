a57bb683d35cbae412d54f27c47115360d832177
luckyantony <luckyantonyofficial@gmail.com>
2025-03-07 06:18:53 +0300

    Introducing scope to Javascript

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+
+  function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'someone';
+    leastFavoriteCustomer = 'new person'; // This will throw an error
+  }
